32754c1169606d7a2077972f720c320ea6653ccb
Will Binns <will@trek.io>
2016-09-05 19:36:50 -0600

    app: Initialize application

---
+++ README.md
@@ -0,0 +1,8 @@
+# NYSE Stock Ticker
+
+Look up the latest prices for stocks on the NYSE.
+
+## Requirements
+
+- 21 [https://21.co/signup]
+- A symbol for a stock on the NYSE you want to fetch the latest price for.
---
+++ index.py
@@ -0,0 +1,46 @@
+#     _   ____  _______ ______   _______      __            
+#    / | / /\ \/ / ___// ____/  /_  __(_)____/ /_____  _____
+#   /  |/ /  \  /\__ \/ __/      / / / / ___/ //_/ _ \/ ___/
+#  / /|  /   / /___/ / /___     / / / / /__/ ,< /  __/ /    
+# /_/ |_/   /_//____/_____/    /_/ /_/\___/_/|_|\___/_/     
+
+# Author: Will Binns (telegram.me/wbinns)
+# Description: Look up the latest prices for stocks on the NYSE.
+# GitHub Repository: NYSE Ticker (github.com/wbinns/NYSE-Ticker)
+# License: Unlicense (unlicense.org)
+
+# Load libraries
+import requests
+import urllib
+import json
+import yaml
+from flask import Flask, request
+from two1.wallet import Wallet
+from two1.bitserv.flask import Payment
+
+# Init Flask, Wallet and Payment
+app = Flask(__name__)
+wallet = Wallet()
+payment = Payment(app, wallet)
+
+# Add 402
+@app.route('/price')
+@payment.required(720)
+def lookup_string():
+    symbol = request.args.get('symbol')
+    link = requests.get('http://www.google.com/finance/info?q=NSE:'+symbol)
+    return link.text
+
+# Add Manifest
+@app.route('/manifest')
+def docs():
+    '''
+    Serves the app manifest to the 21 crawler.
+    '''
+    with open('manifest.yaml', 'r') as f:
+        manifest_yaml = yaml.load(f)
+    return json.dumps(manifest_yaml)
+
+# Init Host
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port='10115')
